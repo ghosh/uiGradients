7e1c0f424f05484dbb9f4b6e4edb42c1bf1c52f2
i-break-codes <vaibhav@browserstack.com>
2016-11-08 17:28:41 +0530

    fix indentation, add fallthrough for keys to avoid code repitition

--- source/scripts/modules/Keys.js
+++ source/scripts/modules/Keys.js
@@ -13,19 +13,12 @@ window.uiGradients = window.uiGradients || {};
 
       $(document).on('keydown', function(event) {
         switch(event.which) {
+          
+          case 37:
+          case 39:
+            if (pallete.hasClass('is-active')) return;
 
-          case 37: // left
-            if (pallete.hasClass('is-active')) {
-              return;
-            }
-            canvas.trigger('changeDown');
-          break;
-
-          case 39: // right
-            if (pallete.hasClass('is-active')) {
-              return;
-            }
-            canvas.trigger('changeUp');
+            (event.which === 37 )? canvas.trigger('changeDown') : canvas.trigger('changeUp');
           break;
 
           case 16: // shift
