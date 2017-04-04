4b4a8ae9160d4594a58c53cbb6c9390904a5f4b8
Indrashish Ghosh <mail@indrashishghosh.com>
2017-04-05 02:45:54 +0530

    Code cleanup

--- src/main.js
+++ src/main.js
@@ -10,11 +10,6 @@ Vue.use(VueClipboards);
 Vue.use(VueAnalytics, {
   id: 'UA-49604576-2',
   autoTracking: false,
-  // debug: {
-  //   enabled: true,
-  //   trace: false,
-  //   sendHitTask: true,
-  // },
 });
 
 Vue.config.productionTip = false;
@@ -26,9 +21,7 @@ new Vue({
   components: { App },
 });
 
-/* eslint-disable no-console */
-/* eslint-disable no-useless-escape */
-
+/* eslint-disable no-console, no-useless-escape */
 console.log('uigradients.com - Beautiful color gradients for design and code');
 console.log('----------------------------------------------------------------');
 console.log('Have a side project you want to collab on? Get in touch!');
