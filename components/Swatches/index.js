ae1da66d7e503d3fc4b17ad6ab49876c970add58
Indrashish Ghosh <mail@indrashishghosh.com>
2018-03-09 14:03:31 +0530

    Added click to copy for swatches

--- components/Swatches/index.js
+++ components/Swatches/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import Swatch from '@/components/Swatch'
 import Spacing from '@/patterns/Spacing'
 import Arrow from './icons/arrow.svg'
 
@@ -14,38 +15,6 @@ const SwatchesContainer = styled.ul`
   flex: 1;
 `
 
-const Swatch = styled.li`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-  padding: 3px 6px;
-  border-radius: 3px;
-  transition: background 0.2s ease-in-out;
-
-  &:hover {
-    background-color: #eceaea;
-  }
-`
-
-const SwatchPallete = styled.span`
-  display: inline-block;
-  width: 14px;
-  height: 14px;
-  background-color: #32c4da;
-  margin-right: 5px;
-
-  /* Adapt the colours based on prop */
-  background-color: ${ props => props.color };
-
-`
-
-const SwatchColor = styled.span`
-  font-family: "SF Mono Regular", "Source Code Pro", Consolas, monospace;
-  font-size: 15px;
-`
-
 const ArrowContainer = styled.span`
   margin-top: 4px;
 `
@@ -56,12 +25,7 @@ const Swatches = (props) => {
       {props.colors.map((color, index) => {
         return (
           <Fragment key={ color + index }>
-            <Swatch>
-              <SwatchPallete color={ color } />
-              <SwatchColor>
-                {color}
-              </SwatchColor>
-            </Swatch>
+            <Swatch color={ color } />
 
             {index !== props.colors.length - 1 && (
               <Fragment>
