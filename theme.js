ee899aa4a73e47b9c7aee580e9621842f2587911
Indrashish Ghosh <mail@indrashishghosh.com>
2019-08-22 21:29:30 +0530

    Moved theme object to its own file

--- theme.js
+++ theme.js
@@ -6,7 +6,8 @@ const theme = {
     sm: 'min-width: 480px',
     md: 'min-width: 768px',
     lg: 'min-width: 992px',
-    xl: 'min-width: 1200px'
+    xl: 'min-width: 1200px',
+    xxl: 'min-width: 1440px'
   }
 }
 
