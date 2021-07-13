f03fb9a12a4ec4848b3717e4086e4f548b9500c2
Indrashish Ghosh <mail@indrashishghosh.com>
2021-07-13 18:22:00 +0530

    Css Reset

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import GlobalStyles from '../styles/Global'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
