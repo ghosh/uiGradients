56e8086ca129958e048165aa2e042e0980f731ee
Indrashish Ghosh <mail@indrashishghosh.com>
2021-07-14 20:21:02 +0530

    Added spacer comp

---
+++ web/components/lib/Spacer/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+interface SpacerProps {
+  m?: number,
+  mx?: number,
+  my?: number,
+  mt?: number,
+  mb?: number,
+  mr?: number,
+  ml?: number
+}
+
+const isNumber = (n: any) => {
+  return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+}
+
+const Spacer = styled.div<SpacerProps>`
+  ${({ m = null }) => m && isNumber(m) && css`
+    margin: ${m * 4}px
+  `}
+
+  ${({ mx = null }) => mx && isNumber(mx) && css`
+    margin-left: ${mx * 4}px;
+    margin-right: ${mx * 4}px;
+  `};
+
+  ${({ my = null }) => my && isNumber(my) && css`
+    margin-left: ${my * 4}px;
+    margin-right: ${my * 4}px;
+  `};
+
+  ${({ mt = null }) => mt && isNumber(mt) && css`
+    margin-top: ${mt * 4}px
+  `}
+
+  ${({ mb = null }) => mb && isNumber(mb) && css`
+    margin-bottom: ${mb * 4}px
+  `}
+
+  ${({ ml = null }) => ml && isNumber(ml) && css`
+    margin-left: ${ml * 4}px
+  `}
+
+  ${({ mr = null }) => mr && isNumber(mr) && css`
+    margin-right: ${mr * 4}px
+  `}
+`
+
+Spacer.displayName = 'Spacer'
+
+export default Spacer
