03c437406758339f419c7a316a914d668f15e539
Indrashish Ghosh <mail@ghosh.io>
2023-02-05 20:04:37 +0530

    chore: Updated app for running tests

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 };
 
 export default config;
