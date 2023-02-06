7f6d7b1545d1bfdbf7c3a818c8f9ea675723e6af
Indrashish Ghosh <mail@ghosh.io>
2023-02-06 20:23:35 +0530

    chore: Added logo

---
+++ app/src/modules/Header/Header.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Logo from './Logo.svelte';
+</script>
+
+<Logo />
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,1 +1,6 @@
+<script lang="ts">
+	import Header from '../modules/Header/Header.svelte';
+</script>
+
+<Header />
 <h1>Welcome to SvelteKit</h1>
